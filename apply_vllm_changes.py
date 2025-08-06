dc0ff58bc0ae53d3f7098d5926b4fa2b343280f2
Huzifa1 <huzifa@gmail.com>
2025-08-06 13:41:46 +0200

    Add more precision to graph capturing

--- apply_vllm_changes.py
+++ apply_vllm_changes.py
@@ -7,9 +7,11 @@ def apply_vllm_changes(vllm_path):
     
     target1 = vllm_dir / "model_executor/model_loader/base_loader.py"
     target2 = vllm_dir / "v1/engine/llm_engine.py"
+    target3 = vllm_dir / "v1/worker/gpu_model_runner.py"
 
     shutil.copy2("vllm_files/base_loader.py", target1)
     shutil.copy2("vllm_files/llm_engine.py", target2)
+    shutil.copy2("vllm_files/gpu_model_runner.py", target3)
     print(f"Changes applied!")
 
 if __name__ == "__main__":
