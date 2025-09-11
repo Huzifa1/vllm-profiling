912a71c95ec1d7fbd2535cebb4895a76caae3fb8
Huzaifa Shaaban Kabakibo <huzaifa@mail.uni-paderborn.de>
2025-09-11 15:02:23 +0200

    Merge parse_input.py into train_predictor.py

--- predictor/train_predictor.py
+++ predictor/train_predictor.py
@@ -5,6 +5,7 @@ import joblib
 import argparse
 from pathlib import Path
 import pickle
+import re
 
 def create_predictor_wrt_key(key, metric, data, models_output_dir):
     """Create predictor for key based on metric"""
@@ -51,9 +52,7 @@ def create_constant_predictor(key, data, models_output_dir):
     with open(f"{models_output_dir}/constant_{key}.pkl", "wb") as f:
         pickle.dump(avg, f)
     
-def create_predictors(predictor_input_path, models_output_dir):
-    with open(predictor_input_path, "r") as f:
-        data = json.load(f)
+def create_predictors(predictor_input, models_output_dir):
     
     key_metric_comb = [
         ("size", "load_weights"),
@@ -63,16 +62,57 @@ def create_predictors(predictor_input_path, models_output_dir):
         ("tokenizer_size", "tokenizer_init")
     ]
     for key_metric in key_metric_comb:
-        create_predictor_wrt_key(key_metric[0], key_metric[1], data, models_output_dir)
-    create_predictor_graph_capturing_time(data, models_output_dir)
-    create_constant_predictor("model_init", data, models_output_dir)
+        create_predictor_wrt_key(key_metric[0], key_metric[1], predictor_input, models_output_dir)
+    create_predictor_graph_capturing_time(predictor_input, models_output_dir)
+    create_constant_predictor("model_init", predictor_input, models_output_dir)
+
+def parse_input(avg_comparison_results_path, models_config_path):
+    with open(avg_comparison_results_path, "r") as f:
+        results = json.load(f)
+        
+    with open(models_config_path, "r") as f:
+        models_config = json.load(f)
+     
+    # Prepare the labels   
+    labels = []
+    for l in results["labels"]:
+        # Model name will be always like this *_model_MODELNAME*
+        match = re.search(r"_model_([a-zA-Z0-9.-]+(?:-[0-9.]+[bk])?(?:-[a-zA-Z0-9]+)*)(?=(_|\.))", l)
+        if match:
+            model_name = match.group(1)
+            labels.append(model_name)
+            
+    # Prepare the data
+    data = results["data"]
+    
+    # Update kv_cache_profiling
+    for i in range(len(data["kv_cache_profiling"])):
+        data["kv_cache_profiling"][i] -= data["torch.compile"][i] + data["graph_compile_cached"][i]
+        
+    # Remove unecessary keys
+    del data["model_loading"]
+    del data["torch.compile"]
+    del data["init_engine"]
+    del data["actual_total_time"]
+    
+        
+        
+    output = {
+        "labels": labels,
+        "data": data,
+        "models_config": models_config
+    }
+    
+    return output
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parse input JSON files.")
-    parser.add_argument("--predictor_input_path", help="Path to predictor_input.json", type=Path, required=True)
+    parser = argparse.ArgumentParser(description="Create the predictors")
+    parser.add_argument("--avg_comparison_results", help="Path to avg_comparison_results.json", type=Path, required=True)
+    parser.add_argument("--models_config", help="Path to models_config.json", type=Path, required=True)
     parser.add_argument("--models_output_dir", help="Path to dump output models", type=Path, required=True)
     args = parser.parse_args()
     
-    create_predictors(args.predictor_input_path, args.models_output_dir)    
+    predictor_input = parse_input(args.avg_comparison_results, args.models_config)
+    create_predictors(predictor_input, args.models_output_dir)    
     
     print("Predictor models have been created successfully!")
