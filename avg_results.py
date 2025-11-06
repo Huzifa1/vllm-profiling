Fix KV cache profiling calculation

avg_results.py
@@ -41,16 +41,8 @@ for iteration in sorted(os.listdir(iterations_dir_path)):
                 # This is important to fix actual_total_time later
                 # The idea is that total_time should be the actual sum of the avgs
                 # Then actual_total_time should be the real total_time + averaged additional time 
-                # Same applies for kv_cache_profiling
                 if key == "actual_total_time":
                     val -= content["data"]["total_time"][i]
-                    
-                if key == "kv_cache_profiling":
-                    # It could be null if this is the first time (no cache is available)
-                    if (graph_compile_cached_time := content["data"]["graph_compile_cached"][i]) is None:
-                        graph_compile_cached_time = 0
-                    if content["data"]["torch.compile"][i] is not None:
-                        val -= content["data"]["torch.compile"][i] + graph_compile_cached_time
                 
                 data_accumulator[key][i].append(val)
 
@@ -82,15 +74,13 @@ for i, value in enumerate(aggregated_mean_data["total_time"]):
     framework_bootstrap = sum_up_keys(aggregated_mean_data, i, ["detect_platfrom", "llm_imports", "get_model_info", "worker_init"])
     model_loading = sum_up_keys(aggregated_mean_data, i, ["load_weights", "model_init"])
     torch_compile = sum_up_keys(aggregated_mean_data, i, ["dynamo_transform_time", "graph_compile_general_shape"])
-    kv_cache_profiling = torch_compile + sum_up_keys(aggregated_mean_data, i, ["kv_cache_profiling", "graph_compile_cached"])
-    init_engine = kv_cache_profiling + sum_up_keys(aggregated_mean_data, i, ["graph_capturing"])
+    init_engine = torch_compile + sum_up_keys(aggregated_mean_data, i, ["kv_cache_profiling", "graph_compile_cached", "graph_capturing"])
     total_time = framework_bootstrap + model_loading + init_engine + sum_up_keys(aggregated_mean_data, i, ["tokenizer_init"])
     actual_total_time = total_time + sum_up_keys(aggregated_mean_data, i, ["actual_total_time"])
     
     aggregated_mean_data["framework_bootstrap"][i] = framework_bootstrap
     aggregated_mean_data["model_loading"][i] = model_loading
     aggregated_mean_data["torch.compile"][i] = torch_compile
-    aggregated_mean_data["kv_cache_profiling"][i] = kv_cache_profiling
     aggregated_mean_data["init_engine"][i] = init_engine
     aggregated_mean_data["total_time"][i] = total_time
     aggregated_mean_data["actual_total_time"][i] = actual_total_time

compare_2_avgs.py
@@ -34,20 +34,8 @@ def compare_step(results1, results2, step, verbosity):
     for i, label in enumerate(results1["labels"]):
         model_name = extract_model_name(label)
 
-        
-        if step == "kv_cache_profiling":
-            # Special case: Substract torch.compile and graph_compile_cached
-            if (graph_compile_cached_time1 := results1["data"]["graph_compile_cached"][i]) is None:
-                graph_compile_cached_time1 = 0
-            value1 = results1["data"][step][i] - results1["data"]["torch.compile"][i] - graph_compile_cached_time1
-            
-            if (graph_compile_cached_time2 := results2["data"]["graph_compile_cached"][i]) is None:
-                graph_compile_cached_time2 = 0
-            value2 = results2["data"][step][i] - results2["data"]["torch.compile"][i] - graph_compile_cached_time2
-            
-        else:    
-            value1 = results1["data"][step][i]
-            value2 = results2["data"][step][i]
+        value1 = results1["data"][step][i]
+        value2 = results2["data"][step][i]
             
         speedup = value2 / value1
         sum_speedup += speedup

profile_log.py
@@ -62,6 +62,13 @@ def main(file_name):
     with open(file_name, "r") as f:
         lines = f.readlines()
 
+    worker_init_start = None
+    worker_init_end = None
+    detect_platfrom_start = None
+    detect_platfrom_end = None
+    llm_imports_end = None
+    get_model_info_end = None
+    
     for line in lines:
         if "Loading weights took" in line:
             s = extract_time(line, r"Loading weights took ([\d.]+) seconds")
@@ -107,26 +114,35 @@ def main(file_name):
             s = extract_time(line, r"took ([\d.]+) seconds")
             profiling_results["model_init"] = s
 
-
+        
         # Now for custom logs
         if "No plugins for group" in line:
             detect_platfrom_start = extract_datetime(line)
         elif "detected platform" in line:
             detect_platfrom_end = extract_datetime(line)
-            profiling_results["detect_platfrom"] = (detect_platfrom_end - detect_platfrom_start).total_seconds()
+            if detect_platfrom_start is not None:
+                profiling_results["detect_platfrom"] = (detect_platfrom_end - detect_platfrom_start).total_seconds()
         elif "Available plugins for group" in line:
             llm_imports_end = extract_datetime(line)
-            profiling_results["llm_imports"] = (llm_imports_end - detect_platfrom_end).total_seconds()
+            if detect_platfrom_end is not None:
+                profiling_results["llm_imports"] = (llm_imports_end - detect_platfrom_end).total_seconds()
         elif "Chunked prefill is" in line:
             get_model_info_end = extract_datetime(line)
-            profiling_results["get_model_info"] = (get_model_info_end - llm_imports_end).total_seconds()
+            if llm_imports_end is not None:
+                profiling_results["get_model_info"] = (get_model_info_end - llm_imports_end).total_seconds()
         elif "Waiting for init message" in line:
             worker_init_start = extract_datetime(line)
         elif "Starting to load model" in line:
             worker_init_end = extract_datetime(line)
-            profiling_results["worker_init"] = (worker_init_end - worker_init_start).total_seconds()
+            if worker_init_start is not None:
+                profiling_results["worker_init"] = (worker_init_end - worker_init_start).total_seconds()
             
-            
+    # Fix KV cache profiling
+    # It includes torch.compile + graph_compile_cached  
+    graph_compile_cached = profiling_results["graph_compile_cached"] or 0
+    torch_compile = profiling_results["torch.compile"] or 0
+    if profiling_results["kv_cache_profiling"] is not None:
+        profiling_results["kv_cache_profiling"] -= torch_compile + graph_compile_cached
     
     # Calc actual_total_time as the difference between first and last line
     total_seconds = None
@@ -141,10 +157,11 @@ def main(file_name):
     profiling_results["actual_total_time"] = total_seconds
     
     # Calc framework_bootstrap time
-    profiling_results["framework_bootstrap"] = profiling_results["detect_platfrom"] + \
-        profiling_results["llm_imports"] + \
-        profiling_results["get_model_info"] + \
-        profiling_results["worker_init"]
+    detect_platfrom = profiling_results["detect_platfrom"] or 0
+    llm_imports = profiling_results["llm_imports"] or 0
+    get_model_info = profiling_results["get_model_info"] or 0
+    worker_init = profiling_results["worker_init"] or 0
+    profiling_results["framework_bootstrap"] = detect_platfrom + llm_imports + get_model_info + worker_init
     
     # Calculate total time
     total_time_keys = ["framework_bootstrap", "model_loading", "init_engine", "tokenizer_init"]